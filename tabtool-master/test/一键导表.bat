98b28169b513d43410221c8b700234fc19bf4df7
ji <591370214@qq.com>
2018-10-25 20:47:08 +0800

    Signed-off-by: ji <591370214@qq.com>

---
+++ tabtool-master/test/一键导表.bat
@@ -0,0 +1,1 @@
+"../tabtool/bin/Debug/tabtool.exe" --out_client ../../myPro/Assets/Data/Config/Table/ --out_server ../cpptest/config/ --out_cpp ../cpptest/ --out_cs ../../myPro/Assets/Scripts/Game/Table/ --in_excel ./ --in_tbs ./meta.tbs
